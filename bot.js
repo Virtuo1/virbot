3a3af771eca5bdd9e40b273ccf23179830b71f8f
Virtuo1 <albin.andersson041@gmail.com>
2018-12-22 15:24:37 +0100

    Changes to structure, and minor features

--- bot.js
+++ bot.js
@@ -1,5 +1,10 @@
-const Discord = require('discord.io');
+const Discord = require('discord.js');
 const logger = require('winston');
+const package = require('./package.json');
+const config = require('./config.json');
+
+// Require commands
+const main_cmd = require('./commands/main');
 
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -8,53 +13,26 @@ logger.add(logger.transports.Console, {
 
 logger.level = 'debug';
 
-var bot = new Discord.Client({
-    token: process.env.TOKEN,
-    autorun: true
-});
+// Create bot client & login
+const bot = new Discord.Client();
 
-bot.on('ready', function (evt) {
+bot.login(process.env.TOKEN);
+
+// On ready
+bot.on('ready', () => {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Running on version ' + package.version);
+
+    bot.user.setPresence({
+        game: {
+            name: 'commands',
+            type: 'LISTENING'
+        },
+        status: 'online'
+    });
 });
 
-bot.setPresence({ status: 'online', game: { name: 'with fire.' } });
-
-bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-
-        args = args.splice(1);
-        switch(cmd) {
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-
-            case 'username':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Your username is '+user+'!'
-                })
-            break;
-
-            case 'hitormiss':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'I guess they never miss ***H U H ?***'
-                })
-            break;
-
-            case 'bot':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Creator: Virtuo \nGithub: https://github.com/Virtuo1/virbot/'
-                })
-            break;
-        }
-    }
-});
+// Commands
+bot.on('message', main_cmd);
