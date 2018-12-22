c6960753c7d850df5fb9569b3700ba5b2e1afd54
Virtuo1 <albin.andersson041@gmail.com>
2018-12-22 15:24:37 +0100

    Changes to structure, and minor features

---
+++ commands/main.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js');
+const logger = require('winston');
+const package = require('./package.json');
+const config = require('./config.json');
+
+// Export commands
+module.exports = message => {
+    // Ping command
+    if (message.content === config.prefix + 'ping') {
+        message.channel.send('pong');
+    }
+
+    // Info about the bot
+    if (message.content === config.prefix + 'info') {
+        const embed = new Discord.RichEmbed()
+        .setColor(3447003)
+        .setTitle("Virbot")
+        .setDescription(package.description)
+        .addField("Bot Version", package.version)
+        .addField("Creator", package.author)
+        .setURL("https://github.com/Virtuo1/virbot/")
+        .setThumbnail("https://avatars.githubusercontent.com/u/37273266")
+        message.channel.send({embed})
+    }
+
+    // Flip a coin
+    if (message.content === config.prefix + 'flipcoin') {
+        if (Math.round(Math.random()) == 1) {
+            message.channel.send({embed: {
+                color: 15844367,
+                description: "Heads"
+            }});
+        } else {
+            message.channel.send({embed: {
+                color: 15105570,
+                description: "Tails"
+            }});
+        }
+    }
+
+    // Echoes your username
+    if (message.content === config.prefix + 'username') {
+        message.channel.send('Your username is '+user.username+'!')
+    }
+
+    // DAB
+    if (message.content === config.prefix + 'hitormiss') {
+        message.channel.send('I guess they never miss ***H U H ?***')
+    }
+}
